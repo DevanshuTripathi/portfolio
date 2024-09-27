869a83b0046cdb4dbf165f4eeae5966687e225d6
DevanshuTripathi <aditheprince120@gmail.com>
2024-09-28 01:46:45 +0530

    feat:Add navbar(not styled)

---
+++ src/NavBar.jsx
@@ -0,0 +1,14 @@
+function NavBar() {
+    return (
+        <div >
+            <ul className="flex gap-10 text-lg ">
+                <li>Introduction</li>
+                <li>Skills</li>
+                <li>Projects</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    )
+}
+
+export default NavBar;
