d0bae6de36369754f2a11669860252846504091b
DevanshuTripathi <aditheprince120@gmail.com>
2024-09-28 22:56:22 +0530

    fix: Fixed the mobile responsive navbar(ig)

--- src/Introduction.jsx
+++ src/Introduction.jsx
@@ -1,23 +1,29 @@
 import { Element } from 'react-scroll';
 import profilePic from './images/profilePic.jpeg'
+import { TypeAnimation } from 'react-type-animation';
 
 function Introduction(){
     return (
         <Element name="introduction">
-        <div className="intro">
+        <div className="intro mt-24">
             <div>
             <img className="profile-pic " src={profilePic} />
             </div>
 
             <div className="intro-content">
             <p className="text-3xl md:text-4xl font-bold mb-5">
-                Hi, I am Devanshu Tripathi
+            <TypeAnimation 
+                        sequence={['Hi, I am Devanshu Tripathi', 1000,]}
+                        repeat={0}
+                        curson={true}
+                        wrapper='p'
+                    />
             </p>
             <p className="text-base md:text-lg block">
-                I am a passionate web developer and also a pretty lazy person.
+                I am a passionate web developer and also a pretty enthusiastic person.
                 I like to code and make websites.
                 Average Chess Enjoyer and Music Player.
-
+                Currently Studying at SRM.
             </p>
             </div>
         </div>
--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,9 +1,21 @@
 import { Link } from 'react-scroll';
-
+import { TypeAnimation } from 'react-type-animation';
+import { useState } from 'react';
+ 
 function NavBar() {
+
+    const [isShowing, setShow] = useState(false);
+
+    const toggleMenu = () => {
+        setShow(!isShowing);
+    }
+
     return (
-        <div className="navbar mr-0">
-            <ul className="flex flex-row justify-end gap-10 px-5 py-5 text-xl">
+        <div className="navbar mr-0 fixed overflow-hidden top-0 ">
+            <ul className="hidden text-sm md:flex-row md:text-lg md:flex px-10 gap-10 py-5 nav ">
+                <li>
+                    Devanshu Tripathi
+                </li>
                 <li>
                     <Link to="introduction" smooth={true} duration={500}>
                         Introduction
@@ -25,6 +37,38 @@ function NavBar() {
                     </Link>
                 </li>
             </ul>
+
+
+            <div className="flex items-center justify-center md:hidden">
+            <div className="py-5 text-xl">Devanshu Tripathi</div>
+            <button className="md:hidden absolute right-6 text-2xl" onClick={toggleMenu}>☰</button>
+            <div className="nav-mob-div">
+            <ul className={`${isShowing ? 'flex' : 'hidden'} mt-2 text-sm flex-col gap-5 px-10 py-5 nav-mob`}>
+                <li>
+                    <Link to="introduction" smooth={true} duration={500}>
+                        Introduction
+                    </Link>
+                </li>
+                <li>
+                    <Link to="skills" smooth={true} duration={500}>
+                        Skills
+                    </Link>
+                </li>
+                <li>
+                    <Link to="projects" smooth={true} duration={500}>
+                        Projects
+                    </Link>
+                </li>
+                <li>
+                    <Link to="contact" smooth={true} duration={500}>
+                        Contact
+                    </Link>
+                </li>
+            </ul>
+            </div>
+            </div>
+
+
         </div>
     )
 }
