9eb029b685cf04e865c6b6ccea602e1340ed345b
DevanshuTripathi <aditheprince120@gmail.com>
2024-09-29 16:41:02 +0530

    feat: Add icons rotate animaiton

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -10,20 +10,36 @@ import css from './images/css.png';
 
 function Skills(){
 
+    window.addEventListener("scroll", function() {
+        const skillImages = document.querySelectorAll('.skill-img');
+        
+        skillImages.forEach((img) => {
+            const rect = img.getBoundingClientRect();
+            const inView = rect.top >= 0 && rect.bottom <= window.innerHeight;
+    
+            if (inView) {
+                img.classList.add('rotated');  // Add rotation class when scrolled into view
+            } else {
+                img.classList.remove('rotated');  // Remove class when scrolled out of view
+            }
+        });
+    });
+
     return (
         <Element name="skills">
+            <h1 className="flex justify-center text-4xl font-bold pt-20 ">Skills</h1>
             <div className="skills relative mt-16 ">
-                <ul className=" flex flex-row justify-between px-32 mb-10">
-                    <img src={django} className="rounded-full h-40 bg-white skill-img "></img>
-                    <img src={react} className="rounded-full h-40 bg-white skill-img "></img>
-                    <img src={java} className="rounded-full h-40 bg-white skill-img "></img>
-                    <img src={cpp} className="rounded-full h-40 bg-white skill-img "></img>
+                <ul className=" flex flex-row justify-between px-4 md:px-32 mb-10">
+                    <img src={django} className="rounded-full h-16 w-16 md:h-40 md:w-40 bg-white skill-img "></img>
+                    <img src={react} className="rounded-full h-16 w-16 md:h-40 md:w-40 bg-white skill-img "></img>
+                    <img src={java} className="rounded-full h-16 w-16 md:h-40 md:w-40 bg-white skill-img "></img>
+                    <img src={cpp} className="rounded-full h-16 w-16 md:h-40 md:w-40 bg-white skill-img "></img>
                 </ul>
-                <ul className="flex flex-row justify-between px-32 mb-10">
-                    <img src={python} className="rounded-full h-40 bg-white skill-img "></img>
-                    <img src={javascript} className="rounded-full h-40 bg-white skill-img "></img>
-                    <img src={html} className="rounded-full h-40 bg-white skill-img "></img>
-                    <img src={css} className="rounded-full h-40 bg-white skill-img "></img>
+                <ul className="flex flex-row justify-between px-4 md:px-32 mb-10">
+                    <img src={python} className="rounded-full h-16 w-16 md:h-40 md:w-40 bg-white skill-img "></img>
+                    <img src={javascript} className="rounded-full h-16 w-16 md:h-40 md:w-40 bg-white skill-img "></img>
+                    <img src={html} className="rounded-full h-16 w-16 md:h-40 md:w-40 bg-white skill-img "></img>
+                    <img src={css} className="rounded-full h-16 w-16 md:h-40 md:w-40 bg-white skill-img "></img>
                 </ul>
 
             </div>
